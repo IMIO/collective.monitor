c756edbcbff993b3f8f960549d749d473ec25da3
Benoît Suttor <benoit.suttor@imio.be>
2015-07-15 11:03:19 +0200

    Add count_valid_users, last_login_time, check_smtp and check_upgrade_steps probes

--- collective/monitor/monitor.py
+++ collective/monitor/monitor.py
@@ -1,10 +1,16 @@
-from Zope2 import app as App
+from DateTime import DateTime
+from Zope2 import app
 from zope.component.hooks import setSite
 import os
+from zope.component import getUtility
+from Products.MailHost.interfaces import IMailHost
+from zope.component.interfaces import ComponentLookupError
 
 
-def get_plone_site(connection, container, plone_path=None):
+def get_plone_site(connection, plone_path=None):
     """ Return plone site """
+    container = app()
+    # app = container.unrestrictedTraverse('/')
     if plone_path:
         plone_paths = [path for path in plone_path.split(os.sep) if path]
         for path_name in plone_paths:
@@ -23,7 +29,7 @@ def get_plone_site(connection, container, plone_path=None):
     else:
         result = False
         for obj in container.values():
-            if obj.meta_type == "Folder": # if plone site come from mount point
+            if obj.meta_type == "Folder":  # if plonesite come from mount point
                 for plone in obj.values():
                     if plone.meta_type == "Plone Site":
                         result = plone
@@ -48,10 +54,81 @@ def get_users(context, obj=True):
 
 def count_users(connection, plone_path=None):
     """the total amount of users in your plone site"""
-    app = App()
-    # container = app.unrestrictedTraverse('/')
-    plone_site = get_plone_site(connection, app, plone_path)
+    plone_site = get_plone_site(connection, plone_path)
     if plone_site:
         setSite(plone_site)
         users = get_users(plone_site)
         connection.write(str(len(users)))
+
+
+def count_valid_users(connection, plone_path=None):
+    """Count all users connected since 90 days"""
+    plone_site = get_plone_site(connection, plone_path)
+    if plone_site:
+        setSite(plone_site)
+        users = get_users(plone_site)
+        valid_users = []
+        for user in users:
+            if user.getProperty("last_login_time") > (DateTime() - 90):
+                valid_users.append(user)
+        connection.write(str(len(valid_users)))
+
+
+def last_login_time(connection, plone_path=None):
+    """Get last login time user"""
+    plone_site = get_plone_site(connection, plone_path)
+    if plone_site:
+        setSite(plone_site)
+        users = get_users(plone_site)
+        last_login = ""
+        for user in users:
+            if not last_login:
+                last_login = user.getProperty("last_login_time")
+            if user.getProperty("last_login_time") > last_login:
+                last_login = user.getProperty("last_login_time")
+        connection.write(str(last_login))
+
+
+def check_smtp(connection, plone_path=None):
+    """Check if SMTP is initialize"""
+    plone_site = get_plone_site(connection, plone_path)
+    if plone_site:
+        setSite(plone_site)
+        try:
+            mail_host = getUtility(IMailHost)
+        except ComponentLookupError:
+            mail_host = getattr(plone_site, "MailHost")
+        mail_errors = []
+        if not mail_host.smtp_host:
+            mail_errors.append("incorrect host: {0}".format(mail_host.smtp_host))
+        if mail_host.smtp_port != 25:
+            mail_errors.append("incorrect port")
+        if (hasattr(mail_host, "smtp_userid") and mail_host.smtp_userid) or (
+                hasattr(mail_host, "smtp_uid") and mail_host.smtp_uid):
+            mail_errors.append("user entered")
+        if (hasattr(mail_host, "smtp_pass") and mail_host.smtp_pass) or (
+                hasattr(mail_host, "smtp_pwd") and mail_host.smtp_pwd):
+            mail_errors.append("password entered")
+        if not plone_site.email_from_address or plone_site.email_from_address == "postmaster@localhost":
+            mail_errors.append("bad mail")
+        connection.write(str(", ".join(mail_errors)))
+
+
+def check_upgrade_steps(connection, plone_path=None):
+    """Check if all upgrade steps are ran."""
+    plone_site = get_plone_site(connection, plone_path)
+    not_upgraded = 0
+    if plone_site:
+        setSite(plone_site)
+        ps = plone_site.portal_setup
+        # qit = plone_site.portal_quickinstaller
+        for profile_id in ps.listProfilesWithUpgrades():
+            is_installed = ps.getLastVersionForProfile(profile_id) != 'unknown'
+            if is_installed:
+                # latest_upgrade_step = qit.getLatestUpgradeStep(profile_id)
+                # if latest_upgrade_step != 'unknown':
+                upgrades = ps.listUpgrades(profile_id)
+                if upgrades:
+                    not_upgraded += 1
+
+        connection.write(str(not_upgraded))
